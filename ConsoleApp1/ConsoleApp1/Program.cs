b655c66447b68b76fe4ca8e430cb6e6263468d10
S28001 <S28001@pjwstk.edu.pl>
2024-03-15 09:19:24 +0100

    Average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics.CodeAnalysis;
+using System.Security.AccessControl;
+using System.Threading.Channels;
+
 Console.WriteLine("Hello, World!");
+int[] tab = { 1, 2, 3, 4, 5 };
+Console.WriteLine(Average(tab));
+
+static double Average(int[] num)
+{
+    double sum = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        sum += num[i];
+    }
+
+    return sum / num.Length;
+}
